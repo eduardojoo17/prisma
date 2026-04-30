532f9aac132df98b2604f29bb3f56a6e784719a7
João Eduardo Paiva Da Costa <pet0160428@aluno.firjansenaisesi.com.br>
2026-04-29 21:30:05 -0300

    final authmidleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedError } from "../helpers/apiError";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+  if (!authorization) throw new UnauthorizedError("token não fornecido");
+
+  const token = authorization.split("")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_PASS ?? "secret") as {
+      id: number;
+    };
+    req.user_id = payload.id;
+    next();
+  } catch {
+    throw new UnauthorizedError("Token invalido ou expirado");
+  }
+};
