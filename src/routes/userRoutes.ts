8ef316622870a2ecc302f98a263474096430fe9e
João Eduardo Paiva Da Costa <pet0160428@aluno.firjansenaisesi.com.br>
2026-05-05 21:00:25 -0300

    novamudanca

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
 import { UserController } from "../controller/UserController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { roleMiddleware } from "../middlewares/roleMiddleware";
+import { UserRole } from "../entity/User";
 
 const router = Router();
 const userControler = new UserController();
 
-router.post("/", userControler.create);
-router.patch("/:id/toggle", userControler.toggleActive);
-router.patch("/:id", authMiddleware, userControler.update);
 router.get("/", userControler.list);
+router.post("/", userControler.create);
+router.patch("/", authMiddleware, userControler.update);
+router.patch("/:id/toggle", authMiddleware, userControler.toggleActive);
 router.get("/active", userControler.listActive);
 router.get("/:id", userControler.listById);
-router.delete("/:id", userControler.delete);
-
+router.delete("/:id", authMiddleware, userControler.delete);
+router.patch(
+  "/role/:id",
+  authMiddleware,
+  roleMiddleware([UserRole.ADMIN]),
+  userControler.updateRole
+);
 export const userRoutes = router;
